c95b41e4bf4d92c99938a6a84b4f672cb0e8a17f
Viktor Sævarsson <viktor.saevars@gmail.com>
2017-03-31 12:43:45 +0000

    no more janky shit in the filter

--- ui/app/components/flight-list.js
+++ ui/app/components/flight-list.js
@@ -1,9 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  searchKeyword: null,
+  searchKeyword: '',
   flights: null,
-  filteredFlights: null,
+  filteredFlights: Ember.computed('searchKeyword', 'flights', function () {
+    let keyword = this.get('searchKeyword');
+
+    if (keyword == "") {
+      return this.get('flights');
+    }
+
+    // let filtered = this.get('flights').filterBy('number', keyword);
+    return this.get('flights').filter(function (item) {
+      return item.get('date').toLowerCase().indexOf(keyword) > -1 ||
+        item.get('number').toLowerCase().indexOf(keyword) > -1 ||
+        item.get('scheduled_time').toLowerCase().indexOf(keyword) > -1 ||
+        item.get('airline').get('name').toLowerCase().indexOf(keyword) > -1 ||
+        item.get('location').get('name').toLowerCase().indexOf(keyword) > -1 ||
+        item.get('status').get('name').toLowerCase().indexOf(keyword) > -1;
+    });
+  }),
   actions: {
     updateList: function () {
       let keyword = this.get('searchKeyword');
@@ -27,6 +43,6 @@ export default Ember.Component.extend({
     }
   },
   didInsertElement () {
-    this.set('filteredFlights', this.get('flights'));
+    // this.set('filteredFlights', this.get('flights'));
   }
 });
